c065938153fa5f336dfdaac6d6f8fe7b51b9fc29
phucvvt <vovanthanhphuc@gmail.com>
2022-03-16 15:58:13 +0700

    [PhucVVT] English version

--- BusinessObjectLibrary/Status.cs
+++ BusinessObjectLibrary/Status.cs
@@ -2,7 +2,9 @@
 {
     public enum Status
     {
-        ACTIVE = 1,
-        INACTIVE = -1
+        AVAILABLE = 1,
+        OUT_OF_STOCK = 2,
+        ALMOST_OUT_OF_STOCK = 3,
+        BEST_SELLER = 4
     }
 }
